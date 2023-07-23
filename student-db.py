e5a8dcf868e3be3ea31f0cdda7726570ef22b42c
Aadi715 <42634812+Aadi715@users.noreply.github.com>
2023-07-23 21:52:03 +0530

    Add files via upload
    My first commit

---
+++ student-db.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ") # String Input.
+last_name = input("Enter your last name: ") # String Input.
+dob = input("Enter your date of birth: ") # int Input.
+standard = input("Enter your standard: ") # int Input.
+age = int(input("Enter your age: ")) # Integer Input.
+address = input("Enter your address: ") # String Input.
+parents_name = input("Enter your parents name: ") # String Input.
+city = input("Enter your city: ") # String Input.
